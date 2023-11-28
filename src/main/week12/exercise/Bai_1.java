18740f6683aa0ebb52dc024f857a2d466a6deb88
dtpthanh <22021167@vnu.edu.vn>
2023-11-29 02:20:25 +0700

    thanhh

--- src/main/week12/exercise/Bai_1.java
+++ src/main/week12/exercise/Bai_1.java
@@ -1,55 +1,50 @@
+
 import java.util.HashMap;
 
 public class Bai_1 {
-  public static void main(String[] args) {
-    int[] input = {1, -2, 3, -1, 2, -4, 2, 3};
-    System.out.println("Input array: ");
-    printArray(input);
 
-    int[] result = findMaxSubarraySum(input);
-    System.out.println("Maximum subarray sum: " + result[0]);
-    System.out.println("Subarray indices: [" + result[1] + ", " + result[2] + "]");
-  }
+  public static int findMaxSubsequenceSum(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
 
-  // Function to find the maximum subarray sum using Hash Table
-  private static int[] findMaxSubarraySum(int[] nums) {
-    HashMap<Integer, Integer> map = new HashMap<>();
+    HashMap<Integer, Integer> sumIndexMap = new HashMap<>();
     int maxSum = 0;
     int currentSum = 0;
-    int start = -1;
-    int end = -1;
 
     for (int i = 0; i < nums.length; i++) {
       currentSum += nums[i];
 
-      if (currentSum == 0) {
-        // Handle the case where the entire array is the maximum subarray
-        start = 0;
-        end = i;
-        maxSum = currentSum + 1; // Set maxSum to a value greater than 0
-      } else if (map.containsKey(currentSum)) {
-        // Update maxSum and subarray indices if a sum is found in the Hash Table
-        int prevIndex = map.get(currentSum);
-        int subarraySum = currentSum - nums[prevIndex] + nums[i];
-        if (subarraySum > maxSum) {
-          start = prevIndex + 1;
-          end = i;
-          maxSum = subarraySum;
-        }
+      if (currentSum <= 0) {
+        currentSum = 0;
+        sumIndexMap.clear();
+        sumIndexMap.put(0, i + 1);
       } else {
-        // Store the current sum and its index in the Hash Table
-        map.put(currentSum, i);
+        if (!sumIndexMap.containsKey(currentSum)) {
+          sumIndexMap.put(currentSum, i);
+        }
+
+        if (currentSum > maxSum) {
+          maxSum = currentSum;
+        }
       }
     }
 
-    return new int[]{maxSum, start, end};
+    if (maxSum == 0) {
+      int maxNegative = Integer.MIN_VALUE;
+      for (int num : nums) {
+        if (num > maxNegative) {
+          maxNegative = num;
+        }
+      }
+      return maxNegative;
+    }
+    return maxSum;
   }
 
-  // Helper function to print an array
-  private static void printArray(int[] arr) {
-    for (int num : arr) {
-      System.out.print(num + " ");
-    }
-    System.out.println();
+  public static void main(String[] args) {
+    int[] input = {1, -2, 3, -1, 2, -4, 2, 3};
+    int result = findMaxSubsequenceSum(input);
+    System.out.println(result);
   }
 }
